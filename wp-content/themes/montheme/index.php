fea7db097645f6928798ac1103cfb5899f1d3196
Gilles Lengy <gilles.lengy@gmail.com>
2020-09-14 14:50:28 +0200

    index.php base template principal

--- wp-content/themes/montheme/index.php
+++ wp-content/themes/montheme/index.php
@@ -2,9 +2,29 @@
 <html>
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
-    <title>Hello</title>
+    <title><?php wp_title(''); ?></title>
+    <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css">
+    <?php wp_head(); ?>
 </head>
 <body>
-<p>Hello World !</p>
+
+<header id="header">
+    <h1><?php echo bloginfo('name'); ?></h1>
+    <nav id="navigation">
+    </nav>
+</header>
+
+<div id="wrap">
+    <section id="content">
+    </section>
+
+    <aside id="sidebar">
+    </aside>
+</div>
+
+<footer id="footer">
+</footer>
+
+<?php wp_footer(); ?>
 </body>
 </html>
