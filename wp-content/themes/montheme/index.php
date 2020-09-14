52158c5aac06f22f96428687a32e7acf5c5ad20c
Gilles Lengy <gilles.lengy@gmail.com>
2020-09-14 14:48:00 +0200

    index.php hello world

--- wp-content/themes/montheme/index.php
+++ wp-content/themes/montheme/index.php
@@ -1,1 +1,10 @@
-<?php
+<!doctype html>
+<html>
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>Hello</title>
+</head>
+<body>
+<p>Hello World !</p>
+</body>
+</html>
